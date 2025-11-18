0747f001188ea64cbc433f9d7da3c1aa50230c8c
Developer <Lepiozdyx@gmail.com>
2025-11-18 10:01:03 -0300

    Initial Commit

---
+++ Hot33Cozy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hot33Cozy
+//
+//  Created by Alex on 18.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hot33Cozy/Hot33CozyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hot33CozyApp.swift
+//  Hot33Cozy
+//
+//  Created by Alex on 18.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct Hot33CozyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
